6843813a21bbab7d957b38480b74a5be028cc257
Andriy Markus <Andr98m@gmail.com>
2018-04-27 19:06:50 +0300

    copy link functionality

---
+++ Shit happens/js/shithappens.js
@@ -0,0 +1,6 @@
+
+// var optbuttns = document.querySelectorAll(".mystoryoptionsbtn");
+
+// Array.prototype.forEach.call(optbuttns, function(element) {
+// 	element.parentElement.insertBefore(element.import.firstChild, element);
+// });
