25c37b9f9948564f7fc710099b084c5ea5e55258
Andriy Markus <andr98m@gmail.com>
2018-04-29 03:32:12 +0300

    get link done

--- Shit happens/js/shithappens.js
+++ Shit happens/js/shithappens.js
@@ -1,6 +1,28 @@
+function addListener(element, eventName, handler) {
+    if (element.addEventListener) {
+      element.addEventListener(eventName, handler, false);
+    }
+    else if (element.attachEvent) {
+      element.attachEvent('on' + eventName, handler);
+    }
+    else {
+      element['on' + eventName] = handler;
+    }
+  }
 
-// var optbuttns = document.querySelectorAll(".mystoryoptionsbtn");
 
-// Array.prototype.forEach.call(optbuttns, function(element) {
-// 	element.parentElement.insertBefore(element.import.firstChild, element);
-// });
+
+var optbuttns = document.querySelectorAll(".mystoryoptionsbtn");
+
+Array.prototype.forEach.call(optbuttns, function(optbuttn) {
+    var element= optbuttn.parentNode.querySelector(".myoptionscon");
+            addListener(optbuttn,"click", function() {
+                if(element.style.display  == "none"){
+                    element.style.display = "block";
+                }
+                else
+                {
+                    element.style.display =  "none";
+                }
+            });
+});
