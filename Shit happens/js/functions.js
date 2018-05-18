ca02c655029cc9ebdb07af269253140e26769609
Andriy Markus <Andr98m@gmail.com>
2018-05-18 20:08:31 +0300

    fixed some conflicts from previous commits (one function addListener removed, overflow:auto commented for .myStoryHeader and .myStoryFooter, getlink button fixed with proper code)

--- Shit happens/js/functions.js
+++ Shit happens/js/functions.js
@@ -23,35 +23,6 @@ Array.prototype.forEach.call(optbuttns, function(optbuttn) {
 });
 
 
-function addListener(element, eventName, handler) {
-    if (element.addEventListener) {
-      element.addEventListener(eventName, handler, false);
-    }
-    else if (element.attachEvent) {
-      element.attachEvent('on' + eventName, handler);
-    }
-    else {
-      element['on' + eventName] = handler;
-    }
-}
-
-
-var optbuttns = document.querySelectorAll(".mystoryoptionsbtn");
-
-Array.prototype.forEach.call(optbuttns, function(optbuttn) {
-    var element= optbuttn.parentNode.querySelector(".myoptionscon");
-            addListener(optbuttn,"click", function() {
-                if(element.style.display  == "none"){
-                    element.style.display = "block";
-                }
-                else
-                {
-                    element.style.display =  "none";
-                }
-            });
-});
-
-
 
 
 // this code helps to toggle window for setting story lifetime
